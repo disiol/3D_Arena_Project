33916291711b03fbfb4a676c950f1dd3bcb3de9f
disiol <denisimus_games@ukr.net>
2023-05-26 15:47:37 +0300

    made the scene structure

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private int _points = 0;
+        private int _maxHeels = 100;
+        private int _curentHeels = 50;
+        private int _maxPower = 100;
+        private int _curentPower = 50;
+       
+        // Start is called before the first frame update
+       
+         
+        //     • getPower
+        // • setPower
+        // • setHeeles
+        // • getHeeles
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
