15a65fbe4203213e77d6d926a40c23d38cf71f37
disiol <denisimus_games@ukr.net>
2023-05-31 18:30:15 +0300

    GetAccessToPrivate

--- Assets/Tests/TestsPlayMode/Movement/PlayerControllerTests.cs
+++ Assets/Tests/TestsPlayMode/Movement/PlayerControllerTests.cs
@@ -14,6 +14,7 @@ namespace Tests.TestsPlayMode.Movement
         private GameObject _playerObject;
         private PlayerController _playerController;
         private PlayerActionsMap _playerActionsMap;
+        private readonly GetAccessToPrivate _getAccessToPrivate = new GetAccessToPrivate();
 
         [SetUp]
         public void Setup()
@@ -44,7 +45,8 @@ namespace Tests.TestsPlayMode.Movement
             yield return null;
 
             float playerControllerMovementSpeed =
-                (float)GetPrivateFieldValue(typeof(PlayerController), _playerController, "movementSpeed");
+                (float)_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "movementSpeed");
             Vector3 expectedPosition = _playerObject.transform.position +
                                        _playerObject.transform.forward * playerControllerMovementSpeed * Time.deltaTime;
 
@@ -64,7 +66,8 @@ namespace Tests.TestsPlayMode.Movement
             yield return null;
 
             float playerControllerTurnSpeed =
-                (float)GetPrivateFieldValue(typeof(PlayerController), _playerController, "turnSpeed");
+                (float)_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "turnSpeed");
             Quaternion expectedRotation = Quaternion.Euler(0f, playerControllerTurnSpeed * Time.deltaTime, 0f) *
                                           _playerObject.transform.rotation;
 
@@ -82,7 +85,9 @@ namespace Tests.TestsPlayMode.Movement
 
             yield return null;
 
-            bool isAttacking = (bool)GetPrivateFieldValue(typeof(PlayerController), _playerController, "_isAttacking");
+            bool isAttacking =
+                (bool)_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "_isAttacking");
             Assert.IsFalse(isAttacking);
         }
 
@@ -97,7 +102,9 @@ namespace Tests.TestsPlayMode.Movement
 
             yield return null;
 
-            bool isAttacking = (bool)GetPrivateFieldValue(typeof(PlayerController), _playerController, "_isAttacking");
+            bool isAttacking =
+                (bool)_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "_isAttacking");
             Assert.IsFalse(isAttacking);
         }
 
@@ -106,46 +113,52 @@ namespace Tests.TestsPlayMode.Movement
         {
             yield return null;
 
-            bool isAttacking = (bool)GetPrivateFieldValue(typeof(PlayerController), _playerController, "_isAttacking");
+            bool isAttacking =
+                (bool)_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "_isAttacking");
             Assert.IsFalse(isAttacking);
         }
 
 
         [UnityTest]
-        public IEnumerator MoveToRandomPosition_ChangesPlayerPosition()
+        public IEnumerator MovePlayerToSafeLocation_SetsPlayerPositionToValidLocation()
         {
-            // Arrange
+            // Create a mock platform object
+            GameObject platformObject = new GameObject();
 
-
-            GameObject platformObject = new GameObject("Platform")
-            {
-                transform =
-                {
-                    position = Vector3.zero,
-                    localScale = new Vector3(10f, 1f, 10f)
-                }
-            };
-
-            SetPrivateFieldValue(typeof(PlayerController), _playerController, "platform",
+            _getAccessToPrivate.SetPrivateFieldValue(typeof(PlayerController), _playerController, "platform",
                 platformObject.transform);
 
-
-            Vector3 initialPosition = _playerObject.transform.position;
-
-
-            // Act
-            MethodInfo privateMethodMoveToRandomPosition = typeof(PlayerController).GetMethod(
-                "MovePlayerToSafeLocation",
-                BindingFlags.NonPublic | BindingFlags.Instance);
-            privateMethodMoveToRandomPosition.Invoke(_playerController, null);
+            // Create mock enemy objects
+            GameObject enemy1 = new GameObject();
+            GameObject enemy2 = new GameObject();
+            Transform[] enemies = new Transform[2] { enemy1.transform, enemy2.transform };
+            _getAccessToPrivate.SetPrivateFieldValue(typeof(PlayerController), _playerController, "enemies", enemies);
 
 
+            // Call the MovePlayerToSafeLocation() method
+            _getAccessToPrivate.GetPrivateMethod(typeof(PlayerController), "MovePlayerToSafeLocation");
+
+
+            // Ensure that the player's position has changed
+            Assert.AreNotEqual(Vector3.zero, _playerController.transform.position);
+
+            // Ensure that the player's new position is away from all enemies
+            Transform[] enemiesValue =
+                (Transform[])_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "enemies");
+            foreach (Transform enemy in enemiesValue)
+            {
+                float distanceToEnemy = Vector3.Distance(_playerController.transform.position, enemy.position);
+                float enemyRadius =(float) _getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "enemyRadius");
+                Assert.Greater(distanceToEnemy, enemyRadius);
+            }
+
+            // Wait for the next frame to complete the test
             yield return null;
-
-            Vector3 newPosition = _playerObject.transform.position;
-
-            Assert.AreNotEqual(initialPosition, newPosition);
         }
+
 
         [UnityTest]
         public IEnumerator PlayerMovesToSafeLocationWhenAtEdge()
@@ -162,13 +175,13 @@ namespace Tests.TestsPlayMode.Movement
                 }
             };
 
-            SetPrivateFieldValue(typeof(PlayerController), _playerController, "platform",
+            _getAccessToPrivate.SetPrivateFieldValue(typeof(PlayerController), _playerController, "platform",
                 platformObject.transform);
 
             Transform enemy1 = CreateEnemy(new Vector3(2f, 0f, 2f));
             Transform enemy2 = CreateEnemy(new Vector3(-3f, 0f, -3f));
 
-            SetPrivateFieldValue(typeof(PlayerController), _playerController, "enemies",
+            _getAccessToPrivate.SetPrivateFieldValue(typeof(PlayerController), _playerController, "enemies",
                 new Transform[] { enemy1, enemy2 });
 
 
@@ -179,9 +192,9 @@ namespace Tests.TestsPlayMode.Movement
             yield return null; // Wait for one frame to trigger Update()
 
             // Assert
-            var playerControllerEdgeDistance =
-                (float)GetPrivateFieldValue(typeof(PlayerController), _playerController, "edgeDistance");
-            ;
+            float playerControllerEdgeDistance =
+                (float)_getAccessToPrivate.GetPrivateFieldValue(typeof(PlayerController), _playerController,
+                    "edgeDistance");
 
             Assert.IsTrue(Vector3.Distance(_playerObject.transform.position, platformObject.transform.position) >
                           playerControllerEdgeDistance);
@@ -202,41 +215,6 @@ namespace Tests.TestsPlayMode.Movement
         }
 
 
-        //Get access to private
-        private object GetPrivateFieldValue(Type type, object instance, string fieldName)
-        {
-            // Get the private field "myPrivateField"
-            FieldInfo field = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
-
-            // Access the private field value
-            object privateFieldValue = field.GetValue(instance);
-
-            return privateFieldValue;
-        }
-
-
-        private void SetPrivateFieldValue(Type type, object instance, string name, Transform value)
-        {
-            // Get the private field by name
-            FieldInfo fieldInfo = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
-
-            // Set the value of the private field
-            if (fieldInfo != null)
-            {
-                fieldInfo.SetValue(instance, value);
-            }
-        }
-
-        private void SetPrivateFieldValue(Type type, object instance, string name, Transform[] value)
-        {
-            // Get the private field by name
-            FieldInfo fieldInfo = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
-
-            // Set the value of the private field
-            if (fieldInfo != null)
-            {
-                fieldInfo.SetValue(instance, value);
-            }
-        }
+       
     }
 }
